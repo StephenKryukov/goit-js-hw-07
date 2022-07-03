8a3da50ec51a1ca781e6a28809ec8516831f5dce
Stepan Kryukov <stephenkryukov@gmail.com>
2022-07-03 12:51:54 +0300

    second task done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from "./gallery-items.js";
+console.log(galleryItems);
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryItemsRef = galleryItems
+  .map(({preview, original, description}) => {
+    return `<div class = "gallery__item"><a class = "gallery__link" href= "${original}"><img  class = "gallery__image" data-source = "${original}" src = ${preview} alt = "${description}"></a></div>`;
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryItemsRef);
+
+// взяли из https://simplelightbox.com/ (раздел Usage)
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsPosition: "bottom",
+  captionsData: "alt",
+  captionsDelay: 250,
+});
